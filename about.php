c237df48296152aa48e395f56c3796d5e3f8324e
hankerstudio <hankerstudio@hotmail.com>
2014-10-27 17:21:30 +0800

    del all

--- about.php
+++
@@ -1,9 +0,0 @@
-<?php include("top.php");?>
-<div class="subtitle"><span>〉</span><a href='index.php'>首页</a> > 公司简介</div>
-<div class="news">
-  <div class="newsInfo">
-    <div align=center><img src="./Public/images/hk.png" width=50% /><br>扫描二维码直接打开</div>
-    <span>&nbsp;&nbsp;&nbsp;&nbsp;寒客工作室于2003年成立，至今已经有10多年的运营历程。寒客工作室主要经营PHP，JSP，ASP，ASP.NET等各种网络编程网站建设。</span>
-  </div>
-</div>
-<?php include("foot.php");?>
